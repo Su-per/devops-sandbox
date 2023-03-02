53bd4e060ff749880ccc17ca218716060dd7011c
이주원 <juwoon7163@gmail.com>
2023-03-02 15:20:09 +0900

    Delete scripts directory

--- scripts/load_image.sh
+++
@@ -1,1 +0,0 @@
-sudo docker pull 838235863925.dkr.ecr.ap-northeast-2.amazonaws.com/test:latest
--- scripts/start_container.sh
+++
@@ -1,1 +0,0 @@
-sudo docker run -d -p 80:8000 838235863925.dkr.ecr.ap-northeast-2.amazonaws.com/test:latest
