ae1efc7b700e33f6eba3923c55fb29e3f9f987e2
이주원 <juwoon7163@gmail.com>
2023-03-08 13:30:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 from fastapi import FastAPI
 import random
 
-def fib(n):
-    _curr, _next = 0, 1
-    for _ in range(n):
-        _curr, _next = _next, _curr + _next
-    return _curr
-
 r = random.randrange(1,100)
 app = FastAPI()
 
 @app.get("/")
 def index():
-    a = fib(5000)
-    return {"Hello": "World!", "ServerID": r, "fib_5000": a}
+    while True:
+        pass
