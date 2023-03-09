e7323dafdbcd5420bbe2ba8b9d03e58881401972
이주원 <juwoon7163@gmail.com>
2023-03-10 08:37:20 +0900

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,7 @@
-export AWS_DEFAULT_REGION=`aws ssm get-parameters --names "/CodePipeline/AWS_DEFAULT_REGION" --query Parameters[*].Value --output text`
-export AWS_ACCOUNT_ID=`aws ssm get-parameters --names "/CodePipeline/AWS_ACCOUNT_ID" --query Parameters[*].Value --output text`
-export IMAGE_REPO_NAME=`aws ssm get-parameters --names "/CodePipeline/IMAGE_REPO_NAME" --query Parameters[*].Value --output text`
-export IMAGE_TAG=`aws ssm get-parameters --names "/CodePipeline/IMAGE_TAG" --query Parameters[*].Value --output text`
+export AWS_DEFAULT_REGION=`aws ssm get-parameters --names "/CodePipeline/AWS_DEFAULT_REGION" --query Parameters[*].Value --output text --region ap-northeast-2`
+export AWS_ACCOUNT_ID=`aws ssm get-parameters --names "/CodePipeline/AWS_ACCOUNT_ID" --query Parameters[*].Value --output text --region ap-northeast-2`
+export IMAGE_REPO_NAME=`aws ssm get-parameters --names "/CodePipeline/IMAGE_REPO_NAME" --query Parameters[*].Value --output text --region ap-northeast-2`
+export IMAGE_TAG=`aws ssm get-parameters --names "/CodePipeline/IMAGE_TAG" --query Parameters[*].Value --output text --region ap-northeast-2`
 
 aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 
